54e4bfd0fe4293739db35b76f19009026fade294
Ninja <krutilcody@yahoo.com>
2017-03-28 07:16:14 -0500

    Each field is validated and if incorrect, shows a red error message with short description of what's wrong. Changed second page to welcome the new user

--- main.py
+++ main.py
@@ -17,26 +17,47 @@
 import webapp2
 import cgi
 
-header = "<link rel='stylesheet' type='text/css' href='styles.css'><h3>Sign&#45;Up</h3>"
-
+header ="<h3>Sign&#45;Up</h3>"
+errorcolor = "style = 'color:red'"
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         passerror = self.request.get('passerror')
-        if passerror:
-            passerror_esc = "<span class = 'error'>"+cgi.escape(passerror)+"</span>"
+        usernameerror = self.request.get('usernameerror')
+        emailerror = self.request.get('emailerror')
+        passmiss = self.request.get('passmiss')
+        usernameerror_esc = ''
+        passerror_esc = ''
+        emailerror_esc = ''
+        if usernameerror:
+            usernameerror_esc = "<span " + errorcolor + ">" + cgi.escape(usernameerror) + "</span>"
         else:
-            passerror_esc = ''
+            usernameerror_esc = ''
+        if emailerror:
+            emailerror_esc = "<span " + errorcolor + ">" + cgi.escape(emailerror) + "</span>"
+        if passmiss:
+            passerror_esc = "<span " + errorcolor + ">" + cgi.escape(passmiss)+"</span>"
+        else:
+            if passerror:
+                passerror_esc = "<span " + errorcolor + ">"+cgi.escape(passerror)+"</span>"
+            else:
+                passerror_esc = ''
+        username = self.request.get('username')
+        email = self.request.get('email')
+        if not username:
+            username = cgi.escape('')
+            if not email:
+                email = cgi.escape('')
         userform = """
         <label>
         Username
-        <input type = 'text' name='username'/>
-        </label><br>
-        """
+        <input type = 'text' name='username' value ='{1}' />
+        </label>{0}<br>
+        """.format(usernameerror_esc,cgi.escape(username))
         passform = """
         <label>
         Password
         <input type ='password' name='password1'/>
-        </label><br>
+        </label>{0}<br>
         <label>
         Verify Password
         <input type ='password' name='password2'/>
@@ -46,9 +67,9 @@ class MainHandler(webapp2.RequestHandler):
         emailform = """
         <label>
         Email
-        <input type ='email' name='email'/>
-        </label><br>
-        """
+        <input type ='email' name='email' value ='{1}' />
+        </label>{0}<br>
+        """.format(emailerror_esc,cgi.escape(email))
         form = "<form action ='/signedup' method='post'>"+userform+passform+emailform+"<input type ='submit'/></form>"
         page = header + form
         self.response.write(page)
@@ -59,14 +80,29 @@ class SignedUp(webapp2.RequestHandler):
         pass1 = self.request.get('password1')
         pass2 = self.request.get('password2')
         email = self.request.get('email')
-        if pass1 != pass2:
-            self.redirect("/?passerror=Passwords don't match")
+        error = "/?"
+        if not username or not pass1 or not pass2 or not email:
+            if not username:
+                error += "usernameerror=No username given&"
+            if not pass1 or not pass2:
+                error += "passmiss=No password was given&"
+            if not email:
+                error += "emailerror=No email given&"
+            if '@' not in email or '.' not in email:
+                error += "emailerror=Invalid Email address"
+        if pass1 and pass2:
+            if pass1 != pass2:
+                error += "passerror=Passwords don't match&"
+        if ' ' in username:
+            error += "usernameerror=No spaces allowed&"
+        if len(error) > 2:
+            if username and email:
+                error += "username=" + username + "&email=" + email + "&"
+            self.redirect(error)
+
         body = """
-        username {0} <br>
-        pass1 {1} <br>
-        pass2 {2} <br>
-        email {3} <br>
-        """.format(username,pass1,pass2,email)
+        <h3>Welcome, {0}</h3>
+        """.format(username)
         page = header + body
         self.response.write(page)
 
