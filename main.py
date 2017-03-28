de92c29d686826fed12e3e2708b666a906d1c9a2
Ninja <krutilcody@yahoo.com>
2017-03-27 20:26:37 -0500

    Added passwords not the same and css stylesheet for errors

--- main.py
+++ main.py
@@ -15,11 +15,62 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+
+header = "<link rel='stylesheet' type='text/css' href='styles.css'><h3>Sign&#45;Up</h3>"
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        passerror = self.request.get('passerror')
+        if passerror:
+            passerror_esc = "<span class = 'error'>"+cgi.escape(passerror)+"</span>"
+        else:
+            passerror_esc = ''
+        userform = """
+        <label>
+        Username
+        <input type = 'text' name='username'/>
+        </label><br>
+        """
+        passform = """
+        <label>
+        Password
+        <input type ='password' name='password1'/>
+        </label><br>
+        <label>
+        Verify Password
+        <input type ='password' name='password2'/>
+        </label>{0}<br>
+        """.format(passerror_esc)
+
+        emailform = """
+        <label>
+        Email
+        <input type ='email' name='email'/>
+        </label><br>
+        """
+        form = "<form action ='/signedup' method='post'>"+userform+passform+emailform+"<input type ='submit'/></form>"
+        page = header + form
+        self.response.write(page)
+
+class SignedUp(webapp2.RequestHandler):
+    def post(self):
+        username = self.request.get('username')
+        pass1 = self.request.get('password1')
+        pass2 = self.request.get('password2')
+        email = self.request.get('email')
+        if pass1 != pass2:
+            self.redirect("/?passerror=Passwords don't match")
+        body = """
+        username {0} <br>
+        pass1 {1} <br>
+        pass2 {2} <br>
+        email {3} <br>
+        """.format(username,pass1,pass2,email)
+        page = header + body
+        self.response.write(page)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/signedup', SignedUp)
 ], debug=True)
